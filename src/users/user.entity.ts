91cc79d77c820fa7c7c4fb80ad90bc67806f5912
Ykoh <ykoh@student.42seoul.kr>
2021-07-22 00:55:01 +0900

    Fix: User entity 수정
    entity가 단일 파일이라 폴더 밖으로 파일 이동
    칼럼 이름과 속성 변경

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  email: string;
+}
