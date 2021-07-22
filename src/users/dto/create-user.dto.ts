63abedc9034bd796adbcca1a0f6b8ebfc82910d4
Ykoh <ykoh@student.42seoul.kr>
2021-07-22 14:59:42 +0900

    Fix: global validation pipe 등록

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,24 @@
-import { IsString, IsOptional } from 'class-validator';
+import {
+  IsString,
+  IsOptional,
+  MinLength,
+  Matches,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @MinLength(3)
+  @MaxLength(12)
+  @Matches(/^[a-zA-Z0-9_]*$/, {
+    message: 'Name must be alphanumeric!',
+  })
   readonly name: string;
 
   @IsOptional()
   @IsString()
+  @Matches(/\.(gif|jpe?g|tiff?|png|webp|bmp)$/i, {
+    message: 'Not a image file!',
+  })
   readonly avatar?: string;
 }
