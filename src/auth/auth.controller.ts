991ad77d2ed12fea52d2ad93759683563ba95c91
Ykoh <ykoh@student.42seoul.kr>
2021-08-02 18:15:36 +0900

    Feat: logout endpoint 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import {
   ApiExcludeEndpoint,
   ApiOperation,
@@ -9,11 +9,12 @@ import { GetUser } from 'src/users/get-user.decorator';
 import { User } from 'src/users/user.entity';
 import { AuthenticatedGuard } from './guards/authenticated.guard';
 import { FtGuard } from './guards/ft.guard';
+import { Request } from 'express';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  @ApiOperation({ summary: '42 OAuth로 로그인합니다.' })
+  @ApiOperation({ summary: '42 OAuth로 로그인합니다(세션 등록).' })
   @ApiResponse({ status: 200, description: '성공' })
   @ApiResponse({ status: 500, description: '실패' })
   @Get('42')
@@ -34,5 +35,14 @@ export class AuthController {
   @UseGuards(AuthenticatedGuard)
   async test(@GetUser() user: User): Promise<User> {
     return user;
+  }
+
+  @ApiOperation({ summary: '로그아웃합니다(세션 종료).' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @Get('logout')
+  @UseGuards(AuthenticatedGuard)
+  async logOut(@Req() req: Request): Promise<void> {
+    req.logOut();
+    return;
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,29 +24,34 @@ import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @ApiTags('Users')
-@ApiCookieAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiCookieAuth()
   @ApiOperation({ summary: '모든 유저 정보를 가져옵니다.' })
   @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 403, description: '세션 인증 실패' })
   @Get()
   @UseGuards(AuthenticatedGuard)
   getUsers(): Promise<User[]> {
     return this.usersService.getUsers();
   }
 
+  @ApiCookieAuth()
   @ApiOperation({ summary: '나의 정보를 가져옵니다.' })
   @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 403, description: '세션 인증 실패' })
   @Get('me')
   @UseGuards(AuthenticatedGuard)
   getUserByRequestUser(@GetUser() user: User): User {
     return user;
   }
 
+  @ApiCookieAuth()
   @ApiOperation({ summary: 'id로 유저 정보를 가져옵니다.' })
   @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 403, description: '세션 인증 실패' })
   @ApiResponse({ status: 404, description: '유저 없음' })
   @Get(':uuid')
   @UseGuards(AuthenticatedGuard)
@@ -54,8 +59,10 @@ export class UsersController {
     return this.usersService.getUserById(uuid);
   }
 
+  @ApiCookieAuth()
   @ApiOperation({ summary: '중복 닉네임을 확인합니다.' })
   @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 403, description: '세션 인증 실패' })
   @ApiResponse({ status: 404, description: '유저 없음' })
   @Head('name/:name')
   @UseGuards(AuthenticatedGuard)
@@ -77,9 +84,11 @@ export class UsersController {
     return this.usersService.createUser(user, createUserDto);
   }
 
+  @ApiCookieAuth()
   @ApiOperation({ summary: '나의 정보를 수정합니다.' })
   @ApiResponse({ status: 200, description: '성공' })
   @ApiResponse({ status: 400, description: '입력 값 검증 실패' })
+  @ApiResponse({ status: 403, description: '세션 인증 실패' })
   @ApiResponse({ status: 409, description: '데이터(닉네임) 중복' })
   @ApiResponse({ status: 500, description: '업데이트 실패' })
   @Patch('me')
