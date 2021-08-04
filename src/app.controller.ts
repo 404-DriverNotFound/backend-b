3930b6c3d05d55ccc8e906faa24d33e73a2fd58f
Ykoh <ykoh@student.42seoul.kr>
2021-08-04 19:45:32 +0900

    Feat: session 인증여부만 알려주는 API 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { AuthenticatedGuard } from './auth/guards/authenticated.guard';
 
+@ApiTags('Default')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @ApiTags('Default')
   @ApiOperation({ summary: '서버의 실행 상태를 확인합니다.' })
   @ApiResponse({ status: 200, description: '서버가 실행 중' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiCookieAuth()
+  @ApiOperation({ summary: '세션인증 결과를 알려줍니다.' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 403, description: '세션 인증 실패' })
+  @Get('session')
+  @UseGuards(AuthenticatedGuard)
+  hasSession(): void {
+    return;
   }
 }
