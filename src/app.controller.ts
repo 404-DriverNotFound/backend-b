81cd4e04c9e5fd1aa3bbfa20c4c7500da0bc0d45
Ykoh <ykoh@student.42seoul.kr>
2021-08-30 15:22:02 +0900

    Feat: data seed를 추가하는 API 구현
    users, friendships 데이터 seed를 추가함.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { AuthenticatedGuard } from './auth/guards/authenticated.guard';
+import { SecondFactorAuthenticatedGuard } from './auth/guards/second-factor-authenticated.guard';
+import { GetUser } from './users/get-user.decorator';
+import { User } from './users/user.entity';
 
 @ApiTags('Default')
 @Controller()
@@ -12,5 +16,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @ApiOperation({ summary: '데이터베이스 seed를 추가합니다.' })
+  @Get('seed')
+  @UseGuards(AuthenticatedGuard)
+  @UseGuards(SecondFactorAuthenticatedGuard)
+  seed(@GetUser() user: User) {
+    return this.appService.seed(user);
   }
 }
