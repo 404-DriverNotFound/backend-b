4540ea1a9dd5a015f1f8c30805758b7912ed806b
Ykoh <ykoh@student.42seoul.kr>
2021-09-15 18:10:32 +0900

    Feat: 나의 모든 매치를 가져오는 API 추가
    `GET` `/matches` API 추가

--- src/matches/matches.controller.ts
+++ src/matches/matches.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  ApiCookieAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { AuthenticatedGuard } from 'src/auth/guards/authenticated.guard';
+import { SecondFactorAuthenticatedGuard } from 'src/auth/guards/second-factor-authenticated.guard';
+import { GetUser } from 'src/users/get-user.decorator';
+import { User } from 'src/users/user.entity';
+import { Match } from './match.entity';
+import { MatchesService } from './matches.service';
 
+@ApiCookieAuth()
+@ApiTags('Matches')
 @Controller('matches')
-export class MatchesController {}
+@UseGuards(AuthenticatedGuard)
+@UseGuards(SecondFactorAuthenticatedGuard)
+export class MatchesController {
+  constructor(private readonly matchesService: MatchesService) {}
+
+  @ApiOperation({ summary: '모든 매치 정보를 가져옵니다.' })
+  @ApiResponse({ status: 200, description: '성공' })
+  @Get()
+  getMatches(@GetUser() user: User): Promise<Match[]> {
+    // TODO PAGINATION
+    return this.matchesService.getMatches(user);
+  }
+}
--- src/matches/matches.service.ts
+++ src/matches/matches.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { Match } from './match.entity';
+import { MatchesRepository } from './matches.repository';
 
 @Injectable()
-export class MatchesService {}
+export class MatchesService {
+  constructor(
+    @InjectRepository(MatchesRepository)
+    private readonly matchesRepository: MatchesRepository,
+  ) {}
+
+  async getMatches(user: User): Promise<Match[]> {
+    // TODO PAGINATION
+    const matches: Match[] = await this.matchesRepository.find({
+      where: [{ user1: user }, { user2: user }],
+    });
+    return matches;
+  }
+}
