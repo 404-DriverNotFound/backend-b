9cd84d93f9cd140b1d48710425403caa781ad809
Ykoh <ykoh@student.42seoul.kr>
2021-09-16 17:41:15 +0900

    Feat: 업적 추가 및 생성일짜 칼럼 추가

--- src/users/entities/achievement.entity.ts
+++ src/users/entities/achievement.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, CreateDateColumn, Entity, ManyToOne } from 'typeorm';
 import { AchievementName } from '../constants/achievement-name.enum';
 import { User } from './user.entity';
 
@@ -7,11 +7,13 @@ export class Achievement {
   @Column({
     type: 'enum',
     enum: AchievementName,
-    default: AchievementName.NEW_COMMER,
     primary: true,
   })
   name: AchievementName;
 
   @ManyToOne(() => User, { primary: true })
   user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
 }
