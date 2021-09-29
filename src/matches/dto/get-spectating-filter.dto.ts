97c1dd140427741283f8af1269ae4cc5c381e916
Jack-R-lantern <tjdfkr2421@gmail.com>
2021-09-29 19:18:51 +0900

    Refactor: Matches DTO 수정

--- src/matches/dto/get-spectating-filter.dto.ts
+++ src/matches/dto/get-spectating-filter.dto.ts
@@ -1,18 +1,6 @@
-import { ApiProperty, PickType } from '@nestjs/swagger';
-import { IsEnum, IsOptional } from 'class-validator';
-import { MatchType } from '../constants/match-type.enum';
-import { PaginationMatchFilterDto } from './pagination-match-filter.dto';
+import { OmitType } from '@nestjs/swagger';
+import { GetMatchesInfoFilterDto } from './get-matches-info-filter.dto';
 
-export class GetSpectatingFilterDto extends PickType(PaginationMatchFilterDto, [
-  'page',
-  'perPage',
-] as const) {
-  @ApiProperty({
-    example: MatchType.LADDER,
-    required: false,
-    enum: MatchType,
-  })
-  @IsOptional()
-  @IsEnum(MatchType)
-  readonly type?: MatchType;
-}
+export class GetSpectatingFilterDto extends OmitType(GetMatchesInfoFilterDto, [
+  'status',
+] as const) {}
