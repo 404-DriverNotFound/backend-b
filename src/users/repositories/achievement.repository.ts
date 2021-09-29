d594f8559796c5dcde3a2f4a29e3a77357cc08c6
Ykoh <ykoh@student.42seoul.kr>
2021-09-30 05:42:11 +0900

    Chore: 사용하지 않는 파일 삭제

--- src/users/repositories/achievement.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Achievement } from '../entities/achievement.entity';
-
-@EntityRepository(Achievement)
-export class AchievementsRepository extends Repository<Achievement> {}
