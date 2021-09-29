08cb023246f450835c475aa25df5b6b480b89a4d
Jack-R-lantern <tjdfkr2421@gmail.com>
2021-09-29 17:30:12 +0900

    Fix: ready시 user정보 리턴

--- src/events/games/room-manager.service.ts
+++ src/events/games/room-manager.service.ts
@@ -57,8 +57,8 @@ export class RoomManagerService {
     this.roomIds.set(socket0.id, roomId);
     this.roomIds.set(socket1.id, roomId);
     room.readyInit();
-    server.to(socket0.id).emit('ready', PlayerPosition.LEFT, CLIENT_SETTINGS);
-    server.to(socket1.id).emit('ready', PlayerPosition.RIGHT, CLIENT_SETTINGS);
+    server.to(socket0.id).emit('ready', PlayerPosition.LEFT, user1, user2, CLIENT_SETTINGS);
+    server.to(socket1.id).emit('ready', PlayerPosition.RIGHT, user2, user1 ,CLIENT_SETTINGS);
     console.log('Room Created :', roomId);
   }
 
