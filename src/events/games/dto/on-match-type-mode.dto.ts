cfa0fb15b5b67e51a6cd69e8f86fb331fbaa35ab
Jack-R-lantern <tjdfkr2421@gmail.com>
2021-10-01 12:30:40 +0900

    Fix: PR의Change부분 반영

--- src/events/games/dto/on-match-type-mode.dto.ts
+++ src/events/games/dto/on-match-type-mode.dto.ts
@@ -1,13 +1,7 @@
 import { MatchType } from '../../../matches/constants/match-type.enum';
 import { MatchGameMode } from '../../../matches/constants/match-game-mode.enum';
-import { IsEnum, IsNotEmpty } from 'class-validator';
 
-export class OnMatchTypeModeDto {
-  @IsNotEmpty()
-  @IsEnum(MatchType)
-  readonly type!: MatchType;
-
-  @IsNotEmpty()
-  @IsEnum(MatchGameMode)
-  readonly mode!: MatchGameMode;
+export interface OnMatchTypeModeDto {
+  readonly type: MatchType;
+  readonly mode: MatchGameMode;
 }
